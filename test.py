3ecb83f7323285bded545dd7fc09cc00934507ff
DaniyalAhmad2 <151085360+DaniyalAhmad2@users.noreply.github.com>
2025-02-06 23:39:36 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,17 @@ import cv2
 import sys
 
 # Open the video capture device (/dev/video0)
-cap = cv2.VideoCapture("/dev/video0")
+gst_pipeline = (
+    "v4l2src device=/dev/video0 ! "
+    "image/jpeg, width=1920, height=1080, framerate=30/1 ! "
+    "jpegdec ! "
+    "videoconvert ! "
+    "video/x-raw,format=BGR ! "
+    "queue max-size-buffers=1 leaky=downstream ! "
+    "appsink drop=true max-buffers=1"
+)
+# Initialize Video Capture
+cap = cv2.VideoCapture(gst_pipeline)
 
 if not cap.isOpened():
     print("Cannot open camera")
